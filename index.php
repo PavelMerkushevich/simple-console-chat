c3d3e18715a1a6d2b9b653acedf5a671bf458f7d
PaulMerkushevich <11mpa@mail.ru>
2021-12-06 21:39:38 +0300

    Small changes

--- index.php
+++ index.php
@@ -6,7 +6,6 @@
   </head>
   <body>
     <h1>Chat in Console</h1>
-    <img src="file:///D:/OpenServer5.4/domains/yii2-photogallery-module/web/images/photogallery/96.jpg" alt="">
     <script type="text/javascript">
     var conn = new WebSocket('ws://test:8080');
     conn.onopen = function(e) {
